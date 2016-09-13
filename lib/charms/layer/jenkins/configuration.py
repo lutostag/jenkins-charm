6d03a534c4d1b2fa54d90f7167b050306c007a4b
Greg Lutostanski <gregory.lutostanski@canonical.com>
2016-09-13 13:52:08 -0500

    disable 2.x splashscreen

--- lib/charms/layer/jenkins/configuration.py
+++ lib/charms/layer/jenkins/configuration.py
@@ -1,9 +1,10 @@
 import os
 
+from charmhelpers.core import host
 from charmhelpers.core import hookenv
 from charmhelpers.core import templating
 
-from charms.layer.jenkins.paths import CONFIG_FILE
+from charms.layer.jenkins.paths import CONFIG_FILE, HOME
 
 PORT = 8080
 
@@ -24,6 +25,21 @@ class Configuration(object):
         self._hookenv = hookenv
         self._templating = templating
 
+    def _disable_splashscreen(self):
+        """Disable the splash screen for 2.x releases of jenkins"""
+        import pwd
+        import grp
+        uid = pwd.getpwnam('jenkins').pw_uid
+        gid = grp.getgrnam('jenkins').gr_gid
+        splash_version_ran = os.path.join(
+            HOME, 'jenkins.install.InstallUtil.lastExecVersion')
+        version_installed = os.path.join(
+            HOME, 'jenkins.install.UpgradeWizard.state')
+        if not os.path.lexists(splash_version_ran):
+            host.symlink(version_installed, splash_version_ran)
+            os.fchown(splash_version_ran, uid, gid)
+            os.fchown(splash_version_ran, uid, gid)
+
     def bootstrap(self):
         """Generate Jenkins' config, if it hasn't done yet."""
         config = self._hookenv.config()
@@ -39,6 +55,7 @@ class Configuration(object):
         self._templating.render(
             "jenkins-config.xml", CONFIG_FILE, context,
             owner="jenkins", group="nogroup")
+        self._disable_splashscreen()
 
         config["_config-bootstrapped"] = True
 
